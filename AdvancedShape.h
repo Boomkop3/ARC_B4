2c0c4e50210f6ae8f276646eed9ff5aa3526b3e6
Sjors de Bruijn <sjors.de.bruijn@gmail.com>
2020-05-15 17:03:14 +0200

    added default methods to advancedshape

--- AdvancedShape.h
+++ AdvancedShape.h
@@ -1,7 +1,10 @@
 #pragma once
+
 #include "AbstractShape.h"
+
 class AdvancedShape : public AbstractShape
 {
-
+public:
+	AdvancedShape();
+	AdvancedShape(double width, double height, double depth);
 };
-
